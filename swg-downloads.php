f2ea14995a2810ff5688fdec6f203ad0940a723f
Eric Griffiths <eric@ohiowebpro.com>
2022-05-18 16:23:41 -0400

    custom routes and counter added

--- swg-downloads.php
+++ swg-downloads.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: SWG Downloads
  * Description: Simple downloader plugin
- * Version: 1.0.7
+ * Version: 1.0.8
  * Author: Eric Griffiths
  * Author URI: https://stonewallgroup.com
  * License: GPLv2 or later
